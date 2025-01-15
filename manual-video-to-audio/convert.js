20bcf50f7b8b98c539d305ae16db1690142b117b
VanVakarian <void@vslav.dev>
2025-01-15 20:16:56 +0300

    ✨ manual script: video to mp3 converter

---
+++ manual-video-to-audio/convert.js
@@ -0,0 +1,34 @@
+import ffmpegStatic from "ffmpeg-static";
+import ffmpeg from "fluent-ffmpeg";
+import fs from "fs";
+import path from "path";
+
+ffmpeg.setFfmpegPath(ffmpegStatic);
+
+const videoDir = "./video";
+const resultsDir = "./results";
+
+if (!fs.existsSync(resultsDir)) {
+  fs.mkdirSync(resultsDir, { recursive: true });
+}
+
+const videoFiles = fs
+  .readdirSync(videoDir)
+  .filter((file) => file.match(/\.(mp4|avi|mov|mkv)$/i));
+
+if (videoFiles.length === 0) {
+  console.log("No video files found in video directory");
+  process.exit(1);
+}
+
+const inputVideo = path.join(videoDir, videoFiles[0]);
+const outputAudio = path.join(
+  resultsDir,
+  path.basename(videoFiles[0], path.extname(videoFiles[0])) + ".mp3"
+);
+
+ffmpeg(inputVideo)
+  .toFormat("mp3")
+  .on("end", () => console.log("Extraction complete"))
+  .on("error", (err) => console.error("Error:", err.message))
+  .save(outputAudio);
